e449b1c1dd34425e8b89b067830b88ca5d93df32
jaegeun1393 <aiden1393@gmail.com>
2022-12-26 00:38:07 -0800

    UI__update

--- src/group_chat_msg.js
+++ src/group_chat_msg.js
@@ -1,4 +1,11 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import DecoupledEditor from "@ckeditor/ckeditor5-build-decoupled-document";
+import axios from "axios";
+import { CLIENT_URL, BASELINE } from "../util";
+
+import GroupChatRoom from "./group_chat_room";
 
 class GroupChatMsg extends Component {
     constructor(props) {
@@ -6,6 +13,8 @@ class GroupChatMsg extends Component {
         this.state = {
         };
         this.closepanel = this.closepanel.bind(this);
+        this.close_calling = this.close_calling.bind(this);
+        this.showVideoCall = this.showVideoCall.bind(this);
     }
 
     closepanel(bol) {
@@ -14,7 +23,22 @@ class GroupChatMsg extends Component {
             main.className = "relative flex justify-center items-center";
         } else {
             main.className = "relative flex justify-center items-center hidden";
+            var main2 = document.getElementById("calling");
+            main2.className = "fixed bottom-[15px] left-[15px] font-sans text-gray-700 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-80 rounded-3xl w-[300px] justify-center content-center";
         }
+    }
+
+    showVideoCall() {
+        var main3 = document.getElementById("videoCalling");
+        main3.className = "fixed overflow-y-auto w-[350px] h-[800px] right-[30px] top-[100px] felx flex-col";
+    }
+
+    close_calling() {
+        var main2 = document.getElementById("calling");
+        main2.className = "hidden fixed bottom-[15px] left-[15px] font-sans text-gray-700 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-80 rounded-3xl w-[300px] justify-center content-center";
+    
+        var main3 = document.getElementById("videoCalling");
+        main3.className = "hidden fixed overflow-y-auto w-[350px] h-[800px] right-[30px] top-[100px] felx flex-col";
     }
 
     render() {
@@ -92,6 +116,7 @@ class GroupChatMsg extends Component {
                                 className="block p-4 pl-10 w-full text-sm text-gray-900 bg-white rounded-full border-2 border-gray-300 outline-none"
                                 placeholder="Write your message..." required="" />
                             <div className="absolute top-2.5 right-2.5 flex gap-4 items-center">
+
                                 <button onClick={() => this.closepanel("on")}>
                                     <svg width="24px" height="24px" viewBox="0 0 24 24" id="call" xmlns="http://www.w3.org/2000/svg">
                                         <rect id="Rectangle_4" width="24" height="24" fill="none" />
@@ -182,6 +207,65 @@ class GroupChatMsg extends Component {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+
+                {/* calling */}
+                <div id="calling" className="hidden fixed bottom-[15px] left-[15px] font-sans text-gray-700 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 opacity-80 rounded-3xl w-[300px] justify-center content-center">
+                    <div className="opacity-100 box-border flex flex-col h-full p-6">
+                        <div className="text-l text-white text-center">
+                            Calling...
+                        </div>
+                        <div className="flex-shrink-0 p-3 font-sans text-white text-5xl text-center">
+                            15:22
+                        </div>
+                        <div className="flex flex-row mt-4 font-sans text-white">
+                            <button onClick={() => this.showVideoCall()}>
+                                <svg fill="currentColor" viewBox="0 0 16 16" height="35px" width="35px">
+                                    <path fillRule="evenodd" d="M0 5a2 2 0 012-2h7.5a2 2 0 011.983 1.738l3.11-1.382A1 1 0 0116 4.269v7.462a1 1 0 01-1.406.913l-3.111-1.382A2 2 0 019.5 13H2a2 2 0 01-2-2V5z" />
+                                </svg>
+                            </button>
+
+                            <button onClick={() => this.showVideoCall()} className="pl-[15px]">
+                                <svg viewBox="0 0 24 24" fill="currentColor" height="35px" width="35px">
+                                    <path d="M9 6H5v4h2V8h2m10 2h-2v2h-2v2h4m2 2H3V4h18m0-2H3c-1.11 0-2 .89-2 2v12a2 2 0 002 2h7v2H8v2h8v-2h-2v-2h7a2 2 0 002-2V4a2 2 0 00-2-2" />
+                                </svg>
+                            </button>
+
+                            <button className="pl-[15px]"> 
+                                <svg fill="none" viewBox="0 0 24 24" height="35px" width="35px">
+                                    <path fill="currentColor" fillRule="evenodd" d="M8 11a4 4 0 100-8 4 4 0 000 8zm0-2a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+                                    <path fill="currentColor" d="M11 14a1 1 0 011 1v6h2v-6a3 3 0 00-3-3H5a3 3 0 00-3 3v6h2v-6a1 1 0 011-1h6zM22 11h-6v2h6v-2zM16 15h6v2h-6v-2zM22 7h-6v2h6V7z" />
+                                </svg>
+                            </button>
+                            <button onClick={() => this.close_calling()} className="ml-[50px] rounded-full bg-rose-700 w-[40px] h-[40px]">
+                                <svg className="text-white ml-[6px]" fill="none" viewBox="0 0 15 15" height="25px" width="25px">
+                                    <path stroke="currentColor" d="M13.5 1.5l-4 4m-7-5h2.22a1 1 0 01.97.757l.585 2.345a1 1 0 01-.654 1.19l-1.108.37a1.21 1.21 0 00-.804 1.385 6.047 6.047 0 004.744 4.744 1.21 1.21 0 001.385-.804l.297-.893a1 1 0 011.396-.578l2.416 1.208a1 1 0 01.553.894V12.5a2 2 0 01-2 2h-2c-5.523 0-10-4.477-10-10v-2a2 2 0 012-2zm9 6a3 3 0 110-6 3 3 0 010 6z" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                {/* video calling */}
+                <div className="hidden fixed overflow-y-auto w-[350px] h-[800px] right-[30px] top-[100px] felx flex-col" id="videoCalling">
+                    <div className="w-[300px] h-[200px] bg-gray-900 rounded-2xl text-white m-2">
+                        video
+                    </div>
+                    <div className="w-[300px] h-[200px] bg-gray-900 rounded-2xl text-white m-2">
+                        video
+                    </div>
+                    <div className="w-[300px] h-[200px] bg-gray-900 rounded-2xl text-white m-2">
+                        video
+                    </div>
+                    <div className="w-[300px] h-[200px] bg-gray-900 rounded-2xl text-white m-2">
+                        video
+                    </div>
+                    <div className="w-[300px] h-[200px] bg-gray-900 rounded-2xl text-white m-2">
+                        video
+                    </div>
+                    <div className="w-[300px] h-[200px] bg-gray-900 rounded-2xl text-white m-2">
+                        video
                     </div>
                 </div>
             </>
